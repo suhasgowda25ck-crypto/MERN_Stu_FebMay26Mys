1978ec740f7a6546f8de95f328bf08f0af2ea06f
suhasgowda25ck-crypto <suhasgowda25ck@gmail.com>
2026-03-04 09:03:41 +0530

    Comparison operators

---
+++ W3/D2/p2.js
@@ -0,0 +1,14 @@
+// Comparison Opertors
+console.log("Comparison Opertors")
+let x=5,y=6;
+console.log("x==y: ",x==y);//loose equality
+console.log("x===y:",x===y);//Strict equality
+console.log("x!==y:",x!==y);//loose inequality
+console.log("x!==y:",x!==y);//Strict inequality
+console.log("x>3:",x>3);
+console.log("x<3:",x<3);
+console.log("x>=3:",x>=3);
+console.log("x<=3:",x<=3);
+
+
+
