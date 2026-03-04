46c82c6279eeb298c84b4bf08de8e393bfaf4a2f
suhasgowda25ck-crypto <suhasgowda25ck@gmail.com>
2026-03-04 09:05:47 +0530

    conditional switch type conversion

---
+++ W3/D2/p4.js
@@ -0,0 +1,35 @@
+// Conditional statements
+// let age= 20;
+// if(age < 13){
+//     console.log(Child);
+// }
+// else if(age<18){
+//     console.log("Teenager");
+// }
+// else
+// {
+//     console.log("Adult")
+// }
+
+// Switch Statements
+// console.log("Switch  statements");
+// const day = "friday";
+// switch(day){
+//     case "monday":
+//         console.log("Start of the week");
+//         break;
+//         case "wednesday":
+//             console.log("mid of the week");
+//             break;
+//         case "friday":
+//             console.log("end of work week");
+//             break;
+//         default:
+//             console.log("some day in the week");
+//             break;
+
+// }
+
+//Type conversion
+let n = Number("ABC");
+console.log("n:,",n," Type of n:",typeof n,"isNaN",isNaN(n));
