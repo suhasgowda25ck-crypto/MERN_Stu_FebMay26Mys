37acb5711e55b6bef158e1a2055f28fdf32f4c5a
suhasgowda25ck-crypto <suhasgowda25ck@gmail.com>
2026-03-08 20:59:22 +0530

    While and do-while

---
+++ W3/D3/p2.js
@@ -0,0 +1,15 @@
+//while loop
+console.log("while loop");
+
+let count = 0;
+while (count<5){
+    console.log("count:",count);
+    count++;
+}
+
+//Do-while loop
+let value = 10;
+do{
+    console.log("value:",value);
+    value++;
+}while(value<3);
