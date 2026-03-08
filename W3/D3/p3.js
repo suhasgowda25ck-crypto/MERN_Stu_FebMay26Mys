83139063f09f3d5284487d74c40f14cd979f30c0
suhasgowda25ck-crypto <suhasgowda25ck@gmail.com>
2026-03-08 21:00:16 +0530

    Forward loop and continue

---
+++ W3/D3/p3.js
@@ -0,0 +1,14 @@
+for(let i=0;i<5;i++){ //Forward loop
+    if(i===2){
+    console.log("i",i);
+    break;
+    }
+}
+
+//continue
+for(let i=0;i<5;i++){ //Forward loop
+    if(i===2){
+    console.log("i",i);
+    continue;
+    }
+}
