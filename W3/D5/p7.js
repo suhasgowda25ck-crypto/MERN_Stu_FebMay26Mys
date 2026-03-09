fd92ea657495021a1d0fd2f07d1dc47def03cf26
suhasgowda25ck-crypto <suhasgowda25ck@gmail.com>
2026-03-09 17:36:33 +0530

    Event bubbling and stoppropagation

---
+++ W3/D5/p7.js
@@ -0,0 +1,19 @@
+const outer = document.getElementById("outer");
+const inner = document.getElementById("inner");
+const innerButton = document.getElementById("innerButton");
+
+outer.addEventListener("click", function () {
+    console.log("Outer div clicked");
+});
+
+inner.addEventListener("click", function () {
+    console.log("Inner div clicked");
+});
+
+innerButton.addEventListener("click", function (event) {
+    console.log("Inner button clicked");
+    
+    event.stopPropagation();
+    
+    console.log("Propagation stopped at button");
+});
