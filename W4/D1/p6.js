24edc732cce4959326c1c53a94e73be19b9b3827
suhasgowda25ck-crypto <suhasgowda25ck@gmail.com>
2026-03-10 06:32:57 +0530

    finally and return in catch block

---
+++ W4/D1/p6.js
@@ -0,0 +1,32 @@
+//finally
+// function example(){
+//     try{
+//         console.log("example in try block");
+//         return "Try_RETURN";
+//     }
+//     finally{
+//         console.log("This is printed");
+//     }
+// }
+//  console.log("Example result:",example());
+
+//return in catch block and finally
+function example(){
+    try{
+        try{
+            throw new Error("New error");
+        }
+        catch(e){
+            console.log("Example 1: caught error");
+            throw e;
+        }
+        finally{
+            console.log("Example 1: finally still runs");
+        }
+    }
+    catch(e){
+        console.log("Example 1 outer catch:", e.message);
+    }
+}
+
+console.log("Example 1:", example());
