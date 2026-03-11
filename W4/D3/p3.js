3dac3b4a89c167316d1f5cee1b71d353ebcf68d7
suhasgowda25ck-crypto <suhasgowda25ck@gmail.com>
2026-03-11 12:37:29 +0530

    Saving and reading the sessionStorage

---
+++ W4/D3/p3.js
@@ -0,0 +1,30 @@
+const themeInput = document.getElementById("themeInput");
+const output = document.getElementById("output");
+
+document.getElementById("saveBtn").addEventListener("click",function(){
+    sessionStorage.setItem("theme",themeInput.value);
+     sessionStorage.setItem("userName","Suhas");
+      sessionStorage.setItem("loggedIn","true");
+    console.log("Saved theme:",themeInput.value);
+    output.textContent = "Sucessfully stored to sessionStorage";
+    output.style.color="green";
+})
+
+document.getElementById("readBtn").addEventListener("click",function(){
+    const theme = sessionStorage.getItem("theme");
+    console.log(sessionStorage.getItem("theme",));
+    output.textContent = "theme:"+theme;
+    output.style.color="green";
+})
+
+document.getElementById("removeBtn").addEventListener("click",function(){
+    sessionStorage.removeItem("loggedIn")
+    output.textContent = "Removed 'loggedIn' ";
+    output.style.color="green";
+})
+
+document.getElementById("clearBtn").addEventListener("click", function () {
+    sessionStorage.clear();
+    output.textContent = "sessionStorage cleared";
+    output.style.color = "red";
+})
