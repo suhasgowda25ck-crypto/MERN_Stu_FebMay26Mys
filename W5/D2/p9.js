a16da83ef2f9f155f5a39a1901d1c8cba6d5b554
suhasgowda25ck-crypto <suhasgowda25ck@gmail.com>
2026-03-17 16:19:14 +0530

    promise states: pending , fulfilled , rejected

---
+++ W5/D2/p9.js
@@ -0,0 +1,21 @@
+// promise states: pending , fulfilled , rejected
+const fulfilledPromise = new Promise(function(resolve){
+    console.log("fulfilledPromise is pending");
+
+    setTimeout(function(){
+        resolve("fulfilledPromise is fulfilled");
+    },1000);
+});
+
+const rejectedPromise = new Promise(function(resolve,reject){
+    console.log("rejectedPromise is pending ");
+    setTimeout(function(){
+        reject("rejectedPromise is rejected");
+    },1000)
+});
+fulfilledPromise.then(function(message){
+    console.log(message);
+});
+rejectedPromise.catch(function(message){
+    console.log(message);
+});
