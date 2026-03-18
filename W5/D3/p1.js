f244b43a293980cc9d64bcd6b8869aff1353fd47
suhasgowda25ck-crypto <suhasgowda25ck@gmail.com>
2026-03-18 11:14:29 +0530

    Introduction to async/await

---
+++ W5/D3/p1.js
@@ -0,0 +1,16 @@
+// Introduction to async/await
+function getMessage(){
+    return new Promise(function(resolve){
+        setTimeout(() => {
+            resolve("Async/await makes promise based code easier to read");
+        },300);
+    })
+}
+
+async function showMessage(){
+    console.log("Loading message...");
+    const message = await getMessage();
+    console.log(message);
+}
+
+showMessage();
